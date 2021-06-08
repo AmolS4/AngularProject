2b9905ff1d56f263ee9f27ca0f9dd395acfd4b01
Amol Sawkar <Amol.Sawkar@citiustech.com>
2021-06-08 20:52:48 +0530

    Solution Check in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {LoginModule} from '.././app/components/login/login.module';
+import {RegistrationModule} from '.././app/components/registration/registration.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoginModule,
+    RegistrationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
